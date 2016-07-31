cfb6af495d74f17dc11edba64a1d8eb2df93eea2
fernandorosero <tatofarp@hotmail.com>
2016-08-01 00:04:21 +0200

    Toshiba i5

--- app/src/main/java/com/thelastmonkey/kidlocaliza/util/KidLocalizaUtil.java
+++ app/src/main/java/com/thelastmonkey/kidlocaliza/util/KidLocalizaUtil.java
@@ -1,7 +1,13 @@
 package com.thelastmonkey.kidlocaliza.util;
 
-/**
- * Created by tatof on 31/07/2016.
- */
 public class KidLocalizaUtil {
+
+    public void metodoPrueba(){
+        System.out.println("metodoPrueba()");
+        System.out.println("metodoPrueba()");
+        System.out.println("metodoPrueba()");
+        System.out.println("metodoPrueba()");
+        System.out.println("metodoPrueba()");
+        System.out.println("metodoPrueba()");
+    }
 }
