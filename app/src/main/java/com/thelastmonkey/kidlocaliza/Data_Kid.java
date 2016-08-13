692c882ed5b185ab8fc867130e38abca94596c94
fernandorosero <tatofarp@hotmail.com>
2016-08-13 11:31:09 +0200

    Toshiba

--- app/src/main/java/com/thelastmonkey/kidlocaliza/Data_Kid.java
+++ app/src/main/java/com/thelastmonkey/kidlocaliza/Data_Kid.java
@@ -2,6 +2,7 @@ package com.thelastmonkey.kidlocaliza;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -42,6 +43,8 @@ public class Data_Kid extends AppCompatActivity {
             public void onClick(View view) {
                 //Recojo una preferencia para las preferencias
                 KidLocalizaConstantes.prefs = getPreferences(MODE_APPEND);
+
+                //KidLocalizaConstantes.prefs = PreferenceManager.getDefaultSharedPreferences(getApplication());
 
                 //Se activa la edicion
                 SharedPreferences.Editor editor = KidLocalizaConstantes.prefs.edit();
